2c7a48ab20c710776c2b8b943c34faf647ec0387
Naif Ghannam <dhdfaui@gmail.com>
2025-09-16 13:48:42 +0300

    marge AuthRemoteDataSource into  AuthRepositoryImpl

--- LMD/LMD/Feautures/Login/Data/Repositories/AuthRepositoryImpl.swift
+++ LMD/LMD/Feautures/Login/Data/Repositories/AuthRepositoryImpl.swift
@@ -5,22 +5,28 @@
 //  Created by Naif on 24/03/1447 AH.
 //
 
-final class AuthRepositoryImpl: AuthRepository {
-    private let remote = AuthRemoteDataSource()
+import Foundation
 
+final class AuthRepositoryImpl: AuthRepository {
     func login(email: String, password: String) async throws -> AuthSession {
         let dto = LoginRequestDTO(email: email, password: password)
 
-        let response = try await remote.login(request: dto)
+        let response: LoginResponseDTO = try await NetworkManager.shared.request(
+            endpoint: .login,
+            body: dto
+        )
 
         guard let session = response.toDomain() else {
             throw NetworkError.decodingFailed
         }
-
         return session
     }
 
     func logout() async throws -> Bool {
-        try await remote.logout()
+        struct LogoutResponse: Decodable { let success: Bool }
+        let response: LogoutResponse = try await NetworkManager.shared.request(
+            endpoint: .Logout
+        )
+        return response.success
     }
 }
